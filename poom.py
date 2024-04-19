7817e635cb029cbdbf26fde0393f0f8cfcac76f4
dacryorrhoea <dacryorrhoea@hmail.com>
2024-04-19 01:50:34 -0700

    poom_1 + poom_1.02...

---
+++ poom.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+class MyLineReg():
+    def __init__(self, n_iter, learning_rate, weights):
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+        self.weights = weights
+
+    def __str__(self):
+        return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
+    
+    def fit(self, X, Y):
+        print(Y)
+        X['Y'] = Y
+        print(X)
+        
+
+data = MyLineReg(100, 0.1, {1, 2, 3, 4}) 
+
+input = [['John','Pune'],['Bran','Mumbai'],['Peter','Delhi']] 
+data_frame = pd.DataFrame(input,columns=['Name','City'], index=[1,2,3])
+data_series = pd.Series(['John','Bran','Sam'], index=[1,2,3])
+data.fit(data_frame, data_series)
