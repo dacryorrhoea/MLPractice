fdeaa0e3bcd222ee21ff744e5d79eed1186073da
dacryorrhoea <dacryorrhoea@gmail.com>
2024-04-19 23:00:31 +0300

    =-7955

--- poom.py
+++ poom.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 class MyLineReg():
-    def __init__(self, n_iter, learning_rate, weights):
+    def __init__(self, n_iter, learning_rate, weights=[]):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
@@ -10,11 +10,10 @@ class MyLineReg():
     def __str__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
     
-    def fit(self, X, Y):
-        print(Y)
-        X['Y'] = Y
-        print(X)
-        
+    def fit(self, X, y, verbose=False):
+        X.insert(0, "one", np.ones(X.index.size))
+        for i in range(1, self.n_iter+1):
+            print(i)
 
 data = MyLineReg(100, 0.1, {1, 2, 3, 4}) 
 
